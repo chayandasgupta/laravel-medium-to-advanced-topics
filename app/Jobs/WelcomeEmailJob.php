d14bef2bfd74f45f4354abdc4a3e232c19a575ef
chayandasgupta <chayandas285178@gmail.com>
2022-06-15 11:38:34 +0600

    email and data store using queue, queue with job, queue with delay time

---
+++ app/Jobs/WelcomeEmailJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\SendWelcomeEmail;
+use Illuminate\Support\Facades\Mail;
+
+class WelcomeEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {   
+        Mail::to('chayandas285178@gmail.com')
+        ->send( new SendWelcomeEmail() );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Jobs\StoreUserJob;
+use App\Jobs\WelcomeEmailJob;
+use App\Mail\SendWelcomeEmail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +23,28 @@ Route::get('/', function () {
 
 Route::get('test-notification', [App\Http\Controllers\NotificationTestingController::class, 'sendNotification']);
 Route::get('offer-notification', [App\Http\Controllers\NotificationTestingController::class, 'sendOfferNotification']);
+Route::get('queue-mail', function() {
+    // Mail send without use queue. So, its take huge time for send mail
+    // Mail::to('chayandas285178@gmail.com')->send( new SendWelcomeEmail() );
+
+    // Mail send use queue but without add jobs
+    // dispatch( function() {
+    //     Mail::to('chayandas285178@gmail.com')->send( new SendWelcomeEmail() );
+    // });
+
+    // Mail send useing queue with delay time 
+    // dispatch( function() {
+    //     Mail::to('chayandas285178@gmail.com')
+    //     ->send( new SendWelcomeEmail() );
+    // })->delay(now()->addseconds(5));
+
+    // echo "Mail Sent";
+
+    // Mail send using queue with jobs and delay time
+    dispatch( new WelcomeEmailJob() )->delay(now()->addseconds(5));
+
+    // Now we can store big amount of data in database use queue and job
+    dispatch ( new StoreUserJob() );
+
+    echo "mail send & data store successfully";
+});
